58ea523b6b8fbaedfa7ed15ce422d0a0a8f0edd9
Dmitry <kollafimi2109@mail.ru>
2024-02-25 19:17:46 +0700

    add library and realization window

--- GameEngineCore/includes/GameEngineCore/Utils/test.hpp
+++
@@ -1,5 +0,0 @@
-namespace GameEngine {
-
-	void sayHello();
-
-}
--- GameEngineEditor/src/main.cpp
+++ GameEngineEditor/src/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
-#include <GameEngineCore/Utils/test.hpp>
+#include <memory>
+#include <GameEngineCore/Application.hpp>
+
+class MyApp : public GameEngine::Application
+{
+	virtual void on_update() override
+	{
+		std::cout << "Update frame: " << frame++ << std::endl;
+	}
+
+	int frame = 0;
+};
+
 
 int main()
 {
-	std::cout << "Hello from Game Engine Editor" << std::endl;
+	auto myApp = std::make_unique<MyApp>();
 
-	GameEngine::sayHello();
+	int returnCode = myApp->start(1024, 768, "My chichi App");
 
 	std::cin.get();
+
+	return returnCode;
 }
