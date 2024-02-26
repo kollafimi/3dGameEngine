91d0c2fc68722160417d77cdd7a42dbd146112db
Dmitry <kollafimi2109@mail.ru>
2024-02-26 14:25:42 +0700

    add spdlog and glad

--- GameEngineCore/src/GameEngineCore/Application.cpp
+++ GameEngineCore/src/GameEngineCore/Application.cpp
@@ -1,10 +1,20 @@
 #include "GameEngineCore/Application.hpp"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "GameEngineCore/Log.hpp"
 #include <iostream>
 
 namespace GameEngine {
 	Application::Application()
 	{
+        LOG_INFO("Welcome to spdlog!");
+        LOG_ERROR("Some error message with arg: {}", 1);
+
+        LOG_WARN("Easy padding in numbers like {:08d}", 12);
+        LOG_CRITICAL("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+        LOG_INFO("Support for floats {:03.2f}", 1.23456);
+        LOG_INFO("Positional args are {1} {0}..", "too", "supported");
+        LOG_INFO("{:<30}", "left aligned");
 
 	}
 
@@ -32,11 +42,20 @@ namespace GameEngine {
         /* Make the window's context current */
         glfwMakeContextCurrent(window);
 
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        {
+            LOG_ERROR("FAILED TO INITIALIZE GLAD");
+            return -1;
+        }
+
+        glClearColor(1, 1, 0, 0);
+
+
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
-            //glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT);
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
